c3890f7a02ad66436dd5b83bd9399113a3a7d8bf
Nikhil Vaze <nvaze@electric-cloud.com>
2015-10-11 10:47:51 -0700

    Add Apache 2.0 license

--- src/main/resources/project/azure_driver/Azure.pm
+++ src/main/resources/project/azure_driver/Azure.pm
@@ -1,25 +1,18 @@
-# -------------------------------------------------------------------------
-# Package
-#    Azure.pm
 #
-# Dependencies
-#    Console Application: AzureRequest.exe
+#  Copyright 2015 Electric Cloud, Inc.
 #
-# Purpose
-#    A perl library that encapsulates the logic to perform operations in Windows Azure.
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
 #
-# Plugin Version
-#    1.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
-# Date
-#    09/24/2012
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
 #
-# Engineer
-#    Andres Arias
-#
-# Copyright (c) 2012 Electric Cloud, Inc.
-# All rights reserved
-# -------------------------------------------------------------------------
 
 package Azure;
 
